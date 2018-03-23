ff877a1ddbfdd00b6b9033b6c81661b8f455310c
juanmangd <juanman.gd@gmail.com>
2018-03-23 00:09:39 -0400

    update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
-  firebase: {
-    apiKey: "AIzaSyAwxo8Bt7ZeYfFLI5WrakbNGtJsskBv3wA",
-    authDomain: "oshop-f3445.firebaseapp.com",
-    databaseURL: "https://oshop-f3445.firebaseio.com",
-    projectId: "oshop-f3445",
-    storageBucket: "oshop-f3445.appspot.com",
-    messagingSenderId: "223112932947"    
+  firebase:{
+    apiKey: "AIzaSyCSvCC9_mH3bccNbuyoNFN9RXvjX2Th6Vk",
+    authDomain: "oshop-64ea0.firebaseapp.com",
+    databaseURL: "https://oshop-64ea0.firebaseio.com",
+    projectId: "oshop-64ea0",
+    storageBucket: "",
+    messagingSenderId: "156030994885"
   }
 };
